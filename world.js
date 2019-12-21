f3c352e67effaf4da84d4a07dc354991666f1988
Chris <christran@users.noreply.github.com>
2019-12-21 00:47:25 -0800

    entity replication, the correct way

--- world.js
+++ world.js
@@ -11,6 +11,7 @@ const fs = require('fs');
 const jsonfile = require('jsonfile');
 const chalk = require('chalk');
 
+const tickrate = 10;
 const _config = require('./_config.json');
 const port = _config.worldserver.port;
 
@@ -25,12 +26,11 @@ fs.readdirSync('models').forEach(function(file) {
 // Load Maps.wz into memory
 fs.readdirSync('game/maps/').forEach((file) => {
 	let mapID = path.basename(file, '.json');
-
+	
 	Maps[mapID] = jsonfile.readFileSync('game/maps/' + file);
-	},
+});
 
-	console.log('[Loaded]: Maps.wz')
-);
+console.log('[Loaded] Maps.wz');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -53,15 +53,28 @@ io.on('connection', function (socket) {
 	// Require all Handlers/Factorys
 	const playerHandler = require('./handlers/player-handler')(socket, io, clients);
 	const chatHandler = require('./handlers/chat-handler')(socket, io);
+});
 
 	// Send Client information about other clients in the same map
-	setInterval(() => {
-		let otherClientsInMap = [...clients].filter(searchFor => searchFor.socket != socket.id);
-
-		socket.emit('playersInMap', stuff.arrayToObject(otherClientsInMap.filter(searchFor => searchFor.mapID === parseInt(Object.keys(socket.rooms)[0])), 'name'));
-	}, 1000);
-
-});
+	// Tick = 10hz
+	setInterval(update, 1000 / tickrate);
+	
+	function update () {
+		let activeMaps = Object.keys(io.sockets.adapter.rooms).filter(Number);
+		
+		// Send update to only maps with players in them (SocketIO Rooms)
+		activeMaps.forEach((mapId) => {
+			for (let socketId in io.sockets.adapter.rooms[mapId].sockets) {
+				if (io.sockets.adapter.rooms[mapId]) {
+					io.to(mapId).emit('update', {
+						[io.sockets.connected[socketId].name]: {
+							position: io.sockets.connected[socketId].position
+						}
+					});	
+				}
+			}
+		});
+	}
 
 //Start the Server
 http.listen(port, function () {
